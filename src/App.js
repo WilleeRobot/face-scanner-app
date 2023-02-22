beafdaf10df94f7280bb2c5eaeae1b567425cf23
Will Lee <will@willlee.ca>
2023-02-22 09:44:18 -0700

    Create interactivity on app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,42 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Background from "./components/Background/Background";
 import "./App.css";
+import Clarifai from "clarifai";
 
+const app = new Clarifai.App({
+  apiKey: "2482d8bba84f44ea8f9c1805cf48db78",
+});
+
+console.log(Clarifai);
 class App extends Component {
+  // state to track user-entered url
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+      .predict(
+        {
+          id: "face-detection",
+          name: "face",
+          version: "6dc7e46bc9124c5c8824be4822abe105",
+          type: "visual-detector",
+        },
+        "https://samples.clarifai.com/3o6gb3kkXfLvdKEZs4.gif"
+      )
+      .then(console.log)
+      .catch(console.log);
+  };
+
   render() {
     return (
       <div className="App">
@@ -14,7 +48,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
